ffb3d535a2004b4fe4f6ce8bd380f0270eaaa0f8
codemaster05330 <thomasj0830@gmail.com>
2017-06-14 16:33:41 +0300

    add database credentials

---
+++ includes/dbconfig.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author mohamed hussein
+ * @copyright 2017
+ */
+
+//enable output buffer
+ob_start();
+
+//start session
+session_start();
+
+
+//database credentials
+
+define('DBHOSTNAME','localhost:3306');
+define('DBUSERNAME','root');
+define('DBPASSWORD','');
+define('DBNAME','appstore');
+
+
+?>
