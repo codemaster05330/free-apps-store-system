954f40301736040f69d774df01e6719e9c64364c
codemaster05330 <thomasj0830@gmail.com>
2017-06-15 00:28:39 +0300

    add platform view script

--- admin/platform.php
+++ admin/platform.php
@@ -1,3 +1,6 @@
+<?php
+include_once('../includes/dbconfig.php');
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -8,7 +11,36 @@
 <div id="wrapper">
 <div id="upperPanel"></div>
 <div id="navigationBar"></div>
-<div id="content"></div>
+<div id="content">
+<?php
+if(isset($_GET['action']))
+{
+    
+}
+else
+{
+    $sql="SELECT * FROM platforms";
+    $result=mysql_query($sql) or die("query failed due to ".mysql_error());
+    if(mysql_num_rows($result)==0)
+    {
+        echo 'NO platforms defined yet , <a href="./platform.php?action=new" class="hrefBtn">add new platform</a>';
+    }
+    else
+    {
+        echo'<p><a href="./platform.php?action=new" class="hrefBtn">add new platform</a></p>';
+        echo '<table><tr><th>Platform</th><th>actions</th></tr>';
+        while($row=mysql_fetch_assoc($result))
+        {
+            echo '<tr><td><img id="smallIcon" src="data:image;base64,'.$row['platformIcon'].'">'.$row['platformName'].'</td>';
+            echo '<td><a href="./platform.php?action=edit&id='.$row['platformID'].'" class="hrefBtn">edit</a>';
+            echo '<a href="./platform.php?action=del&id='.$row['platformID'].'" class="hrefBtn">delete</a></td></tr>';
+        }
+        echo '</table>';
+    }
+    
+} 
+?>
+</div>
 <div id="footer"></div>
 </div>
 </body>
