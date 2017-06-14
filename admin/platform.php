48a2d429643e0e11e9c13a0654bb857f997603a3
codemaster05330 <thomasj0830@gmail.com>
2017-06-14 23:46:13 +0300

    add platform page strucure

---
+++ admin/platform.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+</head>
+
+<body>
+<div id="wrapper">
+<div id="upperPanel"></div>
+<div id="navigationBar"></div>
+<div id="content"></div>
+<div id="footer"></div>
+</div>
+</body>
+</html>
