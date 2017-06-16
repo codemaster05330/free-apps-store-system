f212d2162f664797fdfdb0660fec29820a0dfc49
codemaster05330 <thomasj0830@gmail.com>
2017-06-17 00:55:07 +0300

    define global buffers error and success

---
+++ includes/common.functions.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * common functions used in the system
+ * 
+ * @author mohamed hussein 
+ * @copyright 2017
+ */
+
+include_once('dbconfig.php');
+
+//define global error buffer
+$errorBuffer=array();
+//define global success buffer
+$successBuffer=array();
+
+
+?>
