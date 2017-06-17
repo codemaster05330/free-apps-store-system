e7196b98b80fb1a839a32c33059a0b32c09cc382
codemaster05330 <thomasj0830@gmail.com>
2017-06-17 23:18:04 +0300

    create admin side menu elements

---
+++ admin/layout/menu.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * admin navigation menu 
+ * 
+ * @author mohamed hussein 
+ * @copyright 2017
+ */
+ ?>
+ <ul id="adminMenu">
+ <li><a href="./index.php">Dashboard</a></li>
+ <li><a href="./user.php">Users</a></li>
+ <li><a href="./developer.php">Developers</a></li>
+ <li><a href="./category.php">Categories</a></li>
+ <li><a href="./platform.php">Platforms</a></li>
+ <li><a href="./pending.php">Approval Pending</a></li>
+ </ul>
