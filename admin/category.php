176a8dda7d5a6833e68a1537a8a5a3cffe8c92f3
codemaster05330 <thomasj0830@gmail.com>
2017-06-17 17:37:58 +0300

    add cateogry page main strucure

---
+++ admin/category.php
@@ -0,0 +1,20 @@
+<?php
+include_once('category.functions.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+</head>
+
+<body>
+<div id="wrapper">
+<div id="upperPanel"></div>
+<div id="navigationBar"></div>
+<div id="content">
+
+</div>
+<div id="footer"></div>
+</div>
+</body>
+</html>
