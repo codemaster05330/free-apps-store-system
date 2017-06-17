bbb8df11187d55c93eb5ddbb114716d227922fe8
codemaster05330 <thomasj0830@gmail.com>
2017-06-17 16:55:51 +0300

    add editPlatformForm function

--- admin/category.functions.php
+++ admin/category.functions.php
@@ -27,5 +27,53 @@ echo '</select></label><br />
 </form>';
  }
 
+
+/**
+ * create html form for edit an existing category and assign category values to it 
+ * @param int $id category id in categories table
+ */
+ function editPlatformForm($id)
+ {
+    $sql="SELECT * FROM categories WHERE catID=$id";
+            $result=mysql_query($sql) or die("query failed due to ".mysql_error());
+            if(mysql_num_rows($result)==0)//redirect if unknown id 
+            {
+                logError("unkown category id");
+              header("location:./category.php");
+            exit();  
+            }
+            else
+            {//load category content to be edited
+             $row=mysql_fetch_assoc($result);
+             $name=$row['catName'];
+             $parent= $row['catParent'];
+             $id=$row['catID'];
+             echo '<form action="category.php?action=update&id='.$id.'" method="post">
+                <label>Main Category <select name="mainCat"><option value="-1">noune</option>';
+                
+                $sql='SELECT * FROM categories WHERE catParent IS NULL ';
+                $result=mysql_query($sql) or die("query failed due to ".mysql_error());
+                 
+                 while($row=mysql_fetch_assoc($result))
+                 {
+                
+                    if($row['catID']==$parent)
+                    {
+                        echo "<option value=\"{$row['catID']}\" selected=\"selected\" >{$row['catName']}</option>";
+                    }
+                    else
+                    {
+                        echo "<option value=\"{$row['catID']}\">{$row['catName']}</option>";
+                    }
+                }
+            
+                
+                echo '</select></label><br />
+                <label>Category Name :</label><input type="text" name="name" value="'.$name.'"/><br />
+                <input  type="submit" value="submit" name="submit"/>
+                </form>';
+            }
+ }
+    
 ?>
 
