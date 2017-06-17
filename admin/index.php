0cf66256b3b9de6305c6a485146251f24c35ab0d
codemaster05330 <thomasj0830@gmail.com>
2017-06-17 23:19:40 +0300

    create admin index page strucure

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php
+include_once('platform.functions.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+</head>
+<?php
+include_once('./layout/menu.php');
+?>
+<body>
+<div id="wrapper">
+<div id="upperPanel"></div>
+<div id="navigationBar">
+
+</div>
+<div id="content">
+<?php
+?>
+</div>
+<div id="footer"></div>
+</div>
+</body>
+</html>
