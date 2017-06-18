f2bbb4660db12623105fb9c09dc2b326ea8169b7
codemaster05330 <thomasj0830@gmail.com>
2017-06-18 18:16:23 +0300

    create apps page strucure

---
+++ admin/apps.php
@@ -0,0 +1,35 @@
+<?php
+include_once('apps.functions.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+</head>
+
+<body>
+<div id="wrapper">
+<div id="upperPanel">
+<?php
+include_once('./layout/upperPanel.php');
+?>
+</div>
+<div id="navigationBar">
+<?php
+include_once('./layout/menu.php');
+?>
+</div>
+<div id="content">
+<?php
+printError();
+printSuccess();
+?>
+</div>
+<div id="footer">
+<?php
+include_once('./layout/footer.php');
+?>
+</div>
+</div>
+</body>
+</html>
