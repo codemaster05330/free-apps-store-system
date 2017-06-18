6a9da18830c5cb1652a7806bc9f865bc9fea062c
codemaster05330 <thomasj0830@gmail.com>
2017-06-18 18:22:09 +0300

    add apps approve action script

--- admin/apps.php
+++ admin/apps.php
@@ -25,6 +25,17 @@ printError();
 printSuccess();
 if(isset($_GET['action']))
 {
+    switch ($_GET['action'])
+    {
+        case "approve":
+        if(isset($_GET['id']))//redirect if id isn't defined
+        {
+         approveApp($_GET['id'],$_SESSION['id']);   
+        }
+        header("location:./apps.php");
+        exit();
+        break;
+    }
 }
 else
 {
