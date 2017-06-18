158b2d2ae1918432a5c15aa9a07ae8f4051baa42
codemaster05330 <thomasj0830@gmail.com>
2017-06-18 18:37:07 +0300

    add unapproveDeveloper function

--- admin/developers.functions.php
+++ admin/developers.functions.php
@@ -18,6 +18,15 @@ include_once('../includes/common.functions.php');
           mysql_query($sql) or die("query failed due to ".mysql_error());
           logSuccess("developer approved successfully");
  }
-
+ /**
+  * unapprove developer
+  * @param int $id developer id
+  */
+  function unapproveDeveloper($id)
+ {
+    $sql="UPDATE developers SET approvedBy=NULL WHERE developerID=$id ";  
+          mysql_query($sql) or die("query failed due to ".mysql_error());
+          logSuccess("developer unapproved successfully");
+ }
 
 ?>
