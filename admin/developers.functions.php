cae57f71d4abf825a40692a0ac6145ccaca95039
codemaster05330 <thomasj0830@gmail.com>
2017-06-18 18:35:19 +0300

    add approveDeveloper function

---
+++ admin/developers.functions.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * reviews management functions[approve,disapprove,del,display]
+ * 
+ * @author mohamed hussein 
+ * @copyright 2017
+ */
+include_once('../includes/common.functions.php');
+/**
+ * approve developer
+ * @param int $id developer id
+ * @param int $revId reviewer id
+ */
+ function approveDeveloper($id,$revId)
+ {
+    $sql="UPDATE developers SET approvedBy=$revId,approvalDate= NOW() WHERE developerID=$id ";  
+          mysql_query($sql) or die("query failed due to ".mysql_error());
+          logSuccess("developer approved successfully");
+ }
+
+
+?>
