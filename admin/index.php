f7b781fbc7bbdfc34c470a41acced6de08cd942b
codemaster05330 <thomasj0830@gmail.com>
2017-06-18 23:45:20 +0300

    remove quick actions content,include exrernal content

--- admin/index.php
+++ admin/index.php
@@ -21,26 +21,8 @@ include_once('./layout/menu.php');
 </div>
 <div id="content">
 <div id="quickActions">
-<h4>Quick Actions</h4>
-<table>
-<tr><th>Categories</th><th>Platforms</th><th>Users</th><th>Developers</th><th>Apps</th><th>Reviews</th></tr>
-<tr><td><a href="./category.php" class="hrefBtn">all categories</a></td>
-<td><a href="./platform.php"class="hrefBtn">all platforms</a></td>
-<td><a href="./user.php"class="hrefBtn">all users</a></td>
-<td><a href="./developers.php"class="hrefBtn">all developers</a></td>
-<td><a href="./apps.php"class="hrefBtn">all apps</a></td>
-<td><a href="./reviews.php"class="hrefBtn">all reviews</a></td></tr>
-
-<tr><td><a href="./category.php?action=new" class="hrefBtn">New</a></td>
-<td><a href="./platform.php?action=new"class="hrefBtn">New</a></td>
-<td></td>
-<td><a href="./developers.php?action=pending"class="hrefBtn">review pending</a></td>
-<td><a href="./apps.php?action=pending"class="hrefBtn">review pending</a></td>
-<td><a href="./reviews.php?action=pending"class="hrefBtn">review pending</a></td></tr>
-
-</table>
-</div>
 <?php
+include_once('./quickActions.php');
 ?>
 </div>
 <div id="footer">
