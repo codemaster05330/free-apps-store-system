4c9ae966c3f8f1e5226f730626021b1b311da053
codemaster05330 <thomasj0830@gmail.com>
2017-06-18 18:20:56 +0300

    add apps view action script

--- admin/apps.php
+++ admin/apps.php
@@ -23,6 +23,24 @@ include_once('./layout/menu.php');
 <?php
 printError();
 printSuccess();
+if(isset($_GET['action']))
+{
+}
+else
+{
+ $sql='select apps.appID ,apps.appName ,apps.appShortDesc ,apps.appMainCatID,apps.appSubCatID,apps.approvedBy,
+        developers.developerName ,categories.catName from apps,developers,categories where 
+        apps.developerID=developers.developerID and apps.appMainCatID=categories.catID';
+        $result=mysql_query($sql) or die("query failed due to ".mysql_error());
+        if(mysql_num_rows($result)==0)
+        {
+            echo "no apps added yet";
+        }
+        else
+        {
+           displayApps($result);    
+        }        
+}   
 ?>
 </div>
 <div id="footer">
