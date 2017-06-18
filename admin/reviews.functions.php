d7f5282357e2e20fec971eb04ed5d4676de13cfa
codemaster05330 <thomasj0830@gmail.com>
2017-06-18 15:25:06 +0300

    add approveReview function

---
+++ admin/reviews.functions.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * reviews management functions[approve,disapprove,del,display]
+ * 
+ * @author mohamed hussein 
+ * @copyright 2017
+ */
+include_once('../includes/common.functions.php');
+
+/**
+ * approve review
+ * @param int $id review id
+ * @param int $revId reviewer id
+ */
+ function approveReview($id,$revId)
+ {
+    $sql="UPDATE reviews SET approvedBy=$revId,approvalDate= NOW() WHERE reviewID=$id ";  
+          mysql_query($sql) or die("query failed due to ".mysql_error());
+          logSuccess("review approved successfully");
+ }
+ 
+?>
