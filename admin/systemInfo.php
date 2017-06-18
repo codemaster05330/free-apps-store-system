5051f1d2d5d152e6b91569403e09b676b590a7db
codemaster05330 <thomasj0830@gmail.com>
2017-06-19 00:03:04 +0300

    create system pending requests info script

--- admin/systemInfo.php
+++ admin/systemInfo.php
@@ -27,4 +27,26 @@ echo "<tr><td>Users</td><td>$users</td></tr>
 ?>
 </table>
 </div>
+<div id="pendingInfo">
+<table >
+<tr><th colspan="2">Pending Requests</th></tr>
+<?php
+$sql='SELECT * FROM developers WHERE approvedBy IS NULL';
+$result=mysql_query($sql) or die("query failed due to ".mysql_error());
+$developers=mysql_num_rows($result);
+
+$sql='SELECT * FROM apps WHERE approvedBy IS NULL';
+$result=mysql_query($sql) or die("query failed due to ".mysql_error());
+$apps=mysql_num_rows($result);
+
+$sql='SELECT * FROM reviews WHERE approvedBy IS NULL';
+$result=mysql_query($sql) or die("query failed due to ".mysql_error());
+$reviews=mysql_num_rows($result);
+
+echo "<tr><td><a href=\"./developers.php?action=pending\">Developer</a></td><td>$developers</td></tr>
+<tr><td><a href=\"./apps.php?action=pending\">Apps</a></td><td>$apps</td></tr>
+<tr><td><a href=\"./reviews.php?action=pending\">Reviews</a></td><td>$reviews</td></tr>";
+?>
+</table>
+</div>
 </div>
