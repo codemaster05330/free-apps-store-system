12145f8a02d0afffdad7e31d60fcf29582cc97ac
codemaster05330 <thomasj0830@gmail.com>
2017-06-18 18:14:36 +0300

    add displayApps function

--- admin/apps.functions.php
+++ admin/apps.functions.php
@@ -43,4 +43,51 @@ include_once('../includes/common.functions.php');
     logSuccess("app deleted successfully");
   }
  
+ /**
+  * display all apps and management actions
+  * @param result $result query result
+  */
+  function displayApps($result)
+  {
+    echo '<table><tr><th>N</th><th>App</th><th>short description</th><th>App Category</th><th>Author</th>  <th>actions</th></tr>';
+       $count=1;
+        while($row=mysql_fetch_assoc($result))
+        {
+            if($row['approvedBy']==NULL)
+            {
+                echo '<tr id="unapproved">';
+                $action="approve";
+                $actionStr="approve";
+            }
+            else
+            {
+                echo '<tr id="approved">';
+                $action="unapprove";
+                $actionStr="unapprove";
+            }
+            echo '<td>'.$count.'</td>';
+            $count++;
+            echo '<td>'.$row['appName'].'</td>';
+            echo '<td>'.$row['appShortDesc'].'</td>';
+            $mainCat=$row['catName'];
+            $subCat="";
+            if($row['appSubCatID']!=NULL)
+            {
+                $sql="SELECT * FROM categories WHERE catID={$row['appSubCatID']}";
+            $result2=mysql_query($sql) or die("query failed due to ".mysql_error());
+             $row2=mysql_fetch_assoc($result2);
+             $mainCat=$row2['catName'];
+             
+              $subCat=' >> '.$row['catName'];
+            }
+            
+            echo '<td>'.$mainCat.' '.$subCat.'</td>';
+            echo '<td>'.$row['developerName'].'</td>';
+            echo '<td><a href="./apps.php?action='.$action.'&id='.$row['appID'].'" class="hrefBtn">'.$actionStr.'</a>';
+            echo '<a href="./apps.php?action=del&id='.$row['appID'].'" class="hrefBtn">delete</a>';
+             echo '<a href="../app.php?id='.$row['appID'].'" class="hrefBtn">view</a></td></tr>';
+        }
+        echo '</table>';
+  }
+
 ?>
