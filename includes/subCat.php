8f41796a7e31872694e216e61f428c9d54914fa9
codemaster05330 <thomasj0830@gmail.com>
2017-06-20 01:36:01 +0300

    modify method to get and modify response text

--- includes/subCat.php
+++ includes/subCat.php
@@ -1,11 +1,11 @@
 <?php
 include_once('dbconfig.php');
-$id=$_POST['id'];
+$id=$_GET['id'];
 $sql="SELECT * FROM categories WHERE catParent=$id";
  $result=mysql_query($sql) or die("query failed due to ".mysql_error());
  if(mysql_num_rows($result)!=0)
  {
-    echo"<select name='subCat'><option value='-1'>Select Sub Category</option>";
+    echo" Sub Category: <select name='subCat' required><option value='-1'>Select Category</option>";
     while($row=mysql_fetch_assoc($result))
     {
          echo"<option value=\"{$row['catID']}\">{$row['catName']}</option>";
