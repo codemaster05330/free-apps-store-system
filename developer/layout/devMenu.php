e085d0a0264031b097a7a2f03e83643bda1693c6
codemaster05330 <thomasj0830@gmail.com>
2017-06-19 17:10:32 +0300

    create devMenu basic content

---
+++ developer/layout/devMenu.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * admin navigation menu 
+ * 
+ * @author mohamed hussein 
+ * @copyright 2017
+ */
+ ?>
+ <ul id="adminMenu">
+ <li><a href="./index.php">Dashboard</a></li>
+ <li><a href="./newApp.php">new App</a></li>
+ <li><a href="./devApps.php">apps list</a></li>
+ <li><a href="./devReviews.php">Reviews</a></li>
+ </ul>
