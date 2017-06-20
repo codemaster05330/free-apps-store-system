40a84f3003f0410e4e99c7729973e4c9daa16adb
codemaster05330 <thomasj0830@gmail.com>
2017-06-20 18:48:10 +0300

    add form action page and submit button name

--- developer/newAppForm.php
+++ developer/newAppForm.php
@@ -1,4 +1,4 @@
-<form id="newApp" action="" method="post" enctype="multipart/form-data">
+<form id="newApp" action="newApp.php" method="post" enctype="multipart/form-data">
 <fieldset>
 <legend>App Information</legend>
 <p></p><label>Name : </label><input type="text" required name="name"/></p>
@@ -87,7 +87,7 @@ echo '</select></p>';
 </fieldset>
 <fieldset>
 <legend>Finish</legend>
-<input type="submit" value="submit"/> 
+<input type="submit" value="submit" name="submit"/> 
 </fieldset>
 </form>
 <script src="../js/scripts.js"></script>
