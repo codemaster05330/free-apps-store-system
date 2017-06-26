7ad53f312afe12e94d7c70d3711b77c09ecac011
codemaster05330 <thomasj0830@gmail.com>
2017-06-26 18:42:14 +0300

    add developer quick actions script

---
+++ developer/devQuickActions.php
@@ -0,0 +1,7 @@
+<div id="quickActions">
+<h4>Quick Actions</h4>
+<a href="./newApp.php"class="hrefBtn">New App</a>
+<a href="./devApps.php"class="hrefBtn">Apps List</a>
+<a href="./devReviews.php"class="hrefBtn">Reviews</a>
+
+</div>
