2f556e9ace6848ce0f72d4d586fe94cab5e7c6a2
codemaster05330 <thomasj0830@gmail.com>
2017-06-26 17:44:13 +0300

    add update app script

--- developer/editApp.php
+++ developer/editApp.php
@@ -25,8 +25,126 @@ include_once('./layout/devMenu.php');
 <?php
 printError();
 printSuccess();
-if(isset($_POST['update']))
+if(isset($_POST['update'])&&isset($_POST['appID']))
 {
+    $updateSql="";
+    $devID=$_SESSION['id'];
+    $devID=1;
+    $appName=$_POST['name'];
+     if($appName != "")
+     {
+        $updateSql .="appName='$appName',";
+     }
+     
+    $shortDesc=$_POST['shortDesc'];
+    if($shortDesc !="")
+    {
+        $updateSql .="appShortDesc='$shortDesc',";
+    }
+    
+    $longDesc=$_POST['longDesc'];
+    if($longDesc !="")
+    {
+        $updateSql .="applongDesc='$longDesc',";
+    }
+    
+    $appReq=$_POST['requirement'];
+    if($appReq !="")
+    {
+        $updateSql .="appSysRequirements='$appReq',";
+    }
+    
+    $icon=$_FILES['icon']['tmp_name'];
+    if($icon !="")
+    {
+        $icon=addslashes($icon);
+    $icon=file_get_contents($icon);
+    $icon=base64_encode($icon);
+        $updateSql .="appIcon='$icon',";
+    }
+    
+    $appVer=$_POST['version'];
+    if($appVer !="")
+    {
+        $updateSql .="appVersion=$appVer,";
+    }
+    
+    $appRelease=$_POST['release'];
+     if($appRelease !="")
+    {
+        $updateSql .="appReleaseDate='$appRelease',";
+    }
+    
+    $appSize=$_POST['fileSize'];
+    if($appSize !="")
+    {
+        switch($_POST['sizeType'])
+    {
+        case "mb":
+            $appSize *=1024;
+            break;
+         case "gb":
+         $appSize *=1024;
+         $appSize *=1024;
+         break;   
+    }
+        $updateSql .="appSize=$appSize,";
+    }
+    
+    $appPlatform=$_POST['platform'];
+    
+    if($appPlatform !="0")
+    {
+        $updateSql .="appPlatformID=$appPlatform,";
+    }
+    
+    $mainCat=$_POST['mainCat'];
+    if($mainCat !="0")
+    {
+        $updateSql .="appMainCatID=$mainCat,";
+    }
+    
+   // $subCat='NULL';
+    if( isset($_POST['subCat']))
+    {
+        $subCat=$_POST['subCat'];
+        $updateSql .="appSubCatID=$subCat,";
+    }
+    
+    $appLang=$_POST['lang'];
+    
+    if($appLang !="")
+    {
+        $updateSql .="appLanguage='$appLang',";
+    }
+    
+    $screenshots=$_POST['screenShots'];
+    if($screenshots[0] !="")
+    {
+        $updateSql .="appScreenshot1='$screenshots[0]',";
+        $updateSql .="appScreenshot2='$screenshots[1]',";
+        $updateSql .="appScreenshot3='$screenshots[2]',";
+        $updateSql .="appScreenshot4='$screenshots[3]',";
+        $updateSql .="appScreenshot5='$screenshots[4]',";
+    }
+    
+    $links=$_POST['links'];
+    if($links[0] !="")
+    {
+        $updateSql .="appPrimaryLink='$links[0]',";
+        $updateSql .="appSecondaryLink='$links[1]',";
+    }
+    
+    $vedioLink=$_POST['video'];
+    
+    $updateSql .="appVideoLink='$vedioLink',appState=0,approvalMsg='update the app'";
+    
+    $sql ="UPDATE apps SET $updateSql WHERE appID={$_POST['appID']} AND developerID=$devID";
+     mysql_query($sql) or die("query failed due to ".mysql_error());
+     logSuccess($appName."app updated successfuly ");
+     header("location:./devApps.php");
+     exit();
+    
     
 }else
 {
