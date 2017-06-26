092a52c920d1badd38bd6e0b39aeffca6bd1e228
codemaster05330 <thomasj0830@gmail.com>
2017-06-26 17:07:31 +0300

    add edit app structure

---
+++ developer/editApp.php
@@ -0,0 +1,45 @@
+<?php
+include_once('../includes/common.functions.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+</head>
+
+<body>
+<div id="upperPanel">
+<?php
+include_once('./layout/devUpperPanel.php');
+?>
+</div>
+
+<div id="wrapper">
+
+<div id="navigationBar">
+<?php
+include_once('./layout/devMenu.php');
+?>
+</div>
+<div id="content">
+<?php
+printError();
+printSuccess();
+if(isset($_POST['update']))
+{
+    
+}else
+{
+    include_once('editAppForm.php');
+}
+?>
+</div>
+
+</div>
+<div id="footer">
+<?php
+include_once('./layout/devFooter.php');
+?>
+</div>
+</body>
+</html>
