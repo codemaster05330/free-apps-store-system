d893c6a37b42f360f9a1f08d9584e5a56b6c3900
codemaster05330 <thomasj0830@gmail.com>
2017-06-26 14:08:59 +0300

    add unpublishDevApp function

--- developer/devApps.functions.php
+++ developer/devApps.functions.php
@@ -25,11 +25,23 @@ include_once('../includes/common.functions.php');
   * @param int $appID app id
   * @param int $devID developer id
   */ 
-  function publishApp($appID,$devID)
+  function publishDevApp($appID,$devID)
   {
     $sql="UPDATE apps SET appState=1 WHERE appID=$appID AND developerID=$devID ";
     mysql_query($sql) or die("query failed due to ".mysql_error());
     logSuccess("app published successfully");
   }
+  
+  /**
+   * change state of an app from published to unpublished
+   * @param int $appID app id
+   * @param int $devID developer id
+   */ 
+  function unpublishDevApp($appID,$devID)
+  {
+    $sql="UPDATE apps SET appState=3 WHERE appID=$appID AND developerID=$devID ";
+    mysql_query($sql) or die("query failed due to ".mysql_error());
+    logSuccess("app unpublished successfully");
+  } 
   
 ?>
