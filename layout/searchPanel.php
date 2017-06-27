f224851a8301d00d9689fa5bfd3e16a68d026a8c
codemaster05330 <thomasj0830@gmail.com>
2017-06-27 14:26:08 +0300

    add search bar script

---
+++ layout/searchPanel.php
@@ -0,0 +1,6 @@
+<div id="searchBar">
+<form action="" method="get">
+<input type="text" placeholder="type to search" name="keyword"/>
+<input type="submit" name="search" value="search"/>
+</form>
+</div>
