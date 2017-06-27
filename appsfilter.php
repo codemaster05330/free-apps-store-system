12af0ae5537af2597d825c912bc3fcc8a0aeb6d9
codemaster05330 <thomasj0830@gmail.com>
2017-06-27 21:34:19 +0300

    add apps filters sessions variables

---
+++ appsfilter.php
@@ -0,0 +1,59 @@
+<?php
+$appsFilter="";
+$APPSCOUNT=2;
+$sort="";
+
+if(isset($_GET['st']))
+{
+   $offset=$_GET['st']; 
+}
+else
+{
+    $offset=0;
+} 
+
+
+if(isset($_GET['p']))
+{
+    $_SESSION['platform']=$_GET['p'];
+} 
+if(isset($_GET['mc']))
+{
+    $_SESSION['mCat']=$_GET['mc'];
+}
+if(isset($_GET['sc']))
+{
+    $_SESSION['sCat']=$_GET['sc'];
+}
+if(isset($_GET['sort']))
+{
+    $_SESSION['sort']=$_GET['sort'];
+}   
+
+if(isset($_SESSION['platform']))
+{
+ $appsFilter .=" AND appPlatformID={$_SESSION['platform']} ";   
+}
+
+if(isset($_SESSION['mc']))
+{
+  
+    $appsFilter .=" AND appMainCatID={$_SESSION['mc']} ";   
+}
+
+if(isset($_SESSION['sc']))
+{
+   
+    $appsFilter .=" AND appSubCatID={$_SESSION['sc']} ";   
+}
+
+if(isset($_SESSION['sort']))
+{
+    $sort="{$_SESSION['sort']}";  
+}
+else
+{
+    $_SESSION['sort']="appDownloads"; 
+    $sort="appDownloads"; 
+}
+?>
