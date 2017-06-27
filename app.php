bb2f3f79e0eb6e5d2f0e7003241a40a131f94ba8
codemaster05330 <thomasj0830@gmail.com>
2017-06-27 18:00:16 +0300

    add app page structure

---
+++ app.php
@@ -0,0 +1,34 @@
+<?php
+include_once('./includes/common.functions.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>download</title>
+</head>
+
+<body>
+
+<div id="upperPanel">
+<?php
+include_once('./layout/upperBar.php');
+include_once('./layout/searchPanel.php');
+?>
+</div>
+<div id="wrapper">
+<div id="content">
+<?php
+include_once('./appContent.php');
+?>
+</div>
+<div id="sideBars">
+<?php
+//include_once('./layout/topDownloadsBar.php');
+?>
+</div>
+</div>
+<?php
+include_once('./layout/footerBar.php');
+?>
+</body>
+</html>
