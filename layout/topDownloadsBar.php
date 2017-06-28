f2aa0254d1696a15d4272a9515c649882416420c
codemaster05330 <thomasj0830@gmail.com>
2017-06-28 15:10:06 +0300

    change app tabel id to barApp

--- layout/topDownloadsBar.php
+++ layout/topDownloadsBar.php
@@ -7,7 +7,7 @@ $result=mysql_query($sql) or die("query failed due to ".mysql_error());
 while($row=mysql_fetch_assoc($result))
 {
  echo '<tr><td>';
-    echo '<table id="smallApp"><tr><td rowspan="2"><img id="mediumIcon" src="data:image;base64,'.$row['appIcon'].'"></td>';
+    echo '<table id="barApp"><tr><td rowspan="2"><img id="mediumIcon" src="data:image;base64,'.$row['appIcon'].'"></td>';
     echo '<td><a href="../app.php?appID='.$row['appID'].'"><strong>'.$row['appName'].' '.$row['appVersion'].'</strong></a>';
     echo '<p><small>'.$row['appShortDesc'].'</small></p></td></tr>';
     echo '<tr><td><small>Downloads : '.$row['appDownloads'].'</small></td></tr>';
