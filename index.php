762a0a8b26e2131b6213b397bf3e1ceb1be622d3
codemaster05330 <thomasj0830@gmail.com>
2017-06-28 12:50:10 +0300

    add main page layout

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+include_once('./includes/common.functions.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>download</title>
+</head>
+
+<body>
+
+<div id="upperPanel">
+<?php
+include_once('./layout/upperBar.php');
+include_once('./layout/searchPanel.php');
+?>
+</div>
+<div id="wrapper">
+<div id="content">
+<div id="leftSideBar">
+<?php
+include_once('./platforms.php');
+include_once('./categories.php');
+?>
+</div>
+<?php
+include_once('./appsProvider.php');
+?>
+</div>
+<div id="sideBars">
+<?php
+include_once('./layout/topDownloadsBar.php');
+include_once('./layout/recentAppsList.php');
+?>
+</div>
+</div>
+<?php
+include_once('./layout/footerBar.php');
+?>
+</body>
+</html>
