97ea23884437c74b9cfceb0d47391eae15747775
codemaster05330 <thomasj0830@gmail.com>
2017-06-28 14:13:37 +0300

    set limit to 2 and fix table end tag

--- layout/recentAppsList.php
+++ layout/recentAppsList.php
@@ -1,8 +1,8 @@
 <div id="recentAppsList"> 
 <?php
-include_once('../includes/common.functions.php');
+//include_once('../includes/common.functions.php');
 echo '<table><tr><th>Recent Apps</th></tr>';
-$sql="SELECT appID,appName,appIcon,appShortDesc,appVersion,approvalDate FROM apps WHERE appState=1 ORDER BY  approvalDate  DESC LIMIT 10";
+$sql="SELECT appID,appName,appIcon,appShortDesc,appVersion,approvalDate FROM apps WHERE appState=1 ORDER BY  approvalDate  DESC LIMIT 2";
 $result=mysql_query($sql) or die("query failed due to ".mysql_error());
 while($row=mysql_fetch_assoc($result))
 {
@@ -11,7 +11,7 @@ while($row=mysql_fetch_assoc($result))
     echo '<td><a href="../app.php?appID='.$row['appID'].'"><strong>'.$row['appName'].' '.$row['appVersion'].'</strong></a>';
     echo '<p><small>'.$row['appShortDesc'].'</small></p></td></tr>';
     echo '<tr><td><small>at : '.Date('d-m-y',strtotime($row['approvalDate'])).'</small></td></tr>';
-    echo '<table></td></tr>'; 
+    echo '</table></td></tr>'; 
 }
 echo '</table>'; 
 ?>
--- layout/topDownloadsBar.php
+++ layout/topDownloadsBar.php
@@ -1,8 +1,8 @@
 <div id="topDownloadsList"> 
 <?php
-include_once('../includes/common.functions.php');
+//include_once('../includes/common.functions.php');
 echo '<table><tr><th>Top Downloads</th></tr>';
-$sql="SELECT appID,appName,appIcon,appShortDesc,appVersion,appDownloads FROM apps WHERE appState=1 ORDER BY  appDownloads  DESC LIMIT 10";
+$sql="SELECT appID,appName,appIcon,appShortDesc,appVersion,appDownloads FROM apps WHERE appState=1 ORDER BY  appDownloads  DESC LIMIT 2";
 $result=mysql_query($sql) or die("query failed due to ".mysql_error());
 while($row=mysql_fetch_assoc($result))
 {
@@ -11,7 +11,7 @@ while($row=mysql_fetch_assoc($result))
     echo '<td><a href="../app.php?appID='.$row['appID'].'"><strong>'.$row['appName'].' '.$row['appVersion'].'</strong></a>';
     echo '<p><small>'.$row['appShortDesc'].'</small></p></td></tr>';
     echo '<tr><td><small>Downloads : '.$row['appDownloads'].'</small></td></tr>';
-    echo '<table></td></tr>'; 
+    echo '</table></td></tr>'; 
 }
 echo '</table>'; 
 ?>
