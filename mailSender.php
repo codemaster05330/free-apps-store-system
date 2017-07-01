ffa31088c647647dd850168273a915cb425134f6
codemaster05330 <thomasj0830@gmail.com>
2017-07-01 16:57:21 +0300

    add activate action script

---
+++ mailSender.php
@@ -0,0 +1,47 @@
+<?php
+include_once('./includes/common.functions.php');
+
+if(isset($_GET['action']) && isset($_GET['id']))
+{
+    $id=$_GET['id'];
+    
+    if($_GET['action']=='activate')
+    {
+        $sql="SELECT userID,userEmail,userKey FROM users WHERE userID=$id ";
+        $result=mysql_query($sql)or die("query failed ".mysql_error());
+         if(mysql_num_rows($result)==1)
+         {
+           $row=mysql_fetch_assoc($result);
+           $email=$row['userEmail'];
+           $key=$row['userKey'];
+           
+           $to=$email;
+           $subject="Registration Confirmation";
+           $body="<p>Thank you for registering at Free App Store.</p>
+			<p>To activate your account, please click on this link: <a href=\"./activate.php?x=$id&y=$key\">././activate.php?x=$id&y=$key</a></p>
+			<p>your activation key is : $key</p>
+            <p>Regards Site Admin</p>"; 
+            
+            $ret=mail($to,$subject,$body);
+            if($ret==true)
+            {
+                header('location:./activate.php');
+                exit();
+            }
+            else
+            {
+                echo "something went wrong";
+            }
+         }
+      }   
+    else
+    {
+    }
+    
+}
+else
+{
+    header('location:./index.php');
+    exit();
+} 
+?>
