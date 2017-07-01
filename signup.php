c1d68594eb1b7705601d0a04a2ffb339a4474981
codemaster05330 <thomasj0830@gmail.com>
2017-07-01 16:56:23 +0300

    add signup script

--- signup.php
+++ signup.php
@@ -1,10 +1,48 @@
 <?php
 include_once('./includes/common.functions.php');
+if(isset($_POST['signup']))
+{
+    $email=mysql_real_escape_string($_POST['userEmail']);
+    $password1=mysql_real_escape_string($_POST['password1']);
+    $password2=mysql_real_escape_string($_POST['password2']);
+    $firstName=mysql_real_escape_string($_POST['fName']);
+    $lastName=mysql_real_escape_string($_POST['lName']);
+    
+    if($password1 != $password2)
+    {
+        logError("passwords aren't matched.");
+    }
+    else
+    {
+    $password1=md5($password1);
+    
+    $sql="SELECT * FROM users WHERE userEmail='$email'";
+    $result=mysql_query($sql)or die("query failed ".mysql_error());
+    if(mysql_num_rows($result)==1)
+    {
+        logError("already registered email.");
+    }
+    else
+    {
+        $key=md5(uniqid(rand(),true));
+        
+        $sql ="INSERT INTO users (userEmail,userPassword,userFirstName,userLastName,userLevel,joinDate,userKey,userState) VALUES
+                ('$email','$password1','$firstName','$lastName',3,NOW(),'$key',0)";
+        $result=mysql_query($sql)or die("query failed ".mysql_error());
+        logSuccess("account created succeffuly");
+        $id=mysql_insert_id();
+        header("location:./mailSender.php?action=activate&id=$id");
+        exit();
+    }
+    
+    }
+    
+}
 ?>
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>download</title>
+    <title>sign up</title>
     <link rel="stylesheet" href="./styles/mainStyle.css" type="text/css"/>
 </head>
 
