766c4803200c14e3c99bf831cafd4234e4889f63
codemaster05330 <thomasj0830@gmail.com>
2017-07-01 22:09:04 +0300

    add reset password action script

--- mailSender.php
+++ mailSender.php
@@ -34,8 +34,36 @@ if(isset($_GET['action']) && isset($_GET['id']))
             }
          }
       }   
-    else
+    elseif($_GET['action']=='reset')
     {
+         $sql="SELECT userID,userEmail,userKey FROM users WHERE userID=$id ";
+        $result=mysql_query($sql)or die("query failed ".mysql_error());
+         if(mysql_num_rows($result)==1)
+         {
+           $row=mysql_fetch_assoc($result);
+           $email=$row['userEmail'];
+           $key=$row['userKey'];
+           
+           $to=$email;
+           $subject="Password Reset";
+           $body="<p>You requested that the password be reset.</p>
+			<p>If this was a mistake, just ignore this email and nothing will happen.</p>
+			<p>To reset your password, visit the following address: <a href=\"./resetPassword.php?x=$id&y=$key\">./resetPassword.php?x=$id&y=$key</a></p>
+            <p>Regards Site Admin</p>"; 
+            
+            $ret=mail($to,$subject,$body);
+            if($ret==true)
+            {
+                //echo "$body";
+                logSuccess("Please check your inbox for a reset link.");
+                header("location:./resetPassword.php");
+                exit();
+            }
+            else
+            {
+                echo "something went wrong";
+            }
+         }
     }
     
 }
