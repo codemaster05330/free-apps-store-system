55709ea352cf9f15d1f8cf6d1af411fca450fd90
codemaster05330 <thomasj0830@gmail.com>
2017-07-02 16:46:32 +0300

    bind href with Dir variable , redirect after logout

--- layout/upperBar.php
+++ layout/upperBar.php
@@ -1,22 +1,32 @@
 <div id="upperBar">
-<a href="./index.php"><h2>Free Apps Store</h2></a>
+<?php
+if(!isset($Dir))
+{
+    $Dir="./";
+}
+include_once( $Dir.'includes/login.functions.php');
+echo '<a href="'.$Dir.'/index.php"><h2>Free Apps Store</h2></a>';
+?>
 <div id="rightPanel">
 <?php
 //include_once('./includes/login.functions.php');
 if(isset($_GET['logout']))
 {
     logout();
+    header("location:./");
+    exit();
 }
 if(isSignedIn())
 {
     echo 'wellcome '. $_SESSION['firstName'];
+    echo '<a href="'.$Dir.'user/index.php" id="hrefBtn">Profile</a>';
     if($_SESSION['userLevel']==0)
     {
-        echo '<a href="./admin/index.php" id="hrefBtn">Admin Dashboard</a>';
+        echo '<a href="'.$Dir.'admin/index.php" id="hrefBtn">Admin Dashboard</a>';
     }
     elseif($_SESSION['userLevel']==1)
     {
-        echo '<a href="./developer/index.php" id="hrefBtn">Developer Dashboard</a>';
+        echo '<a href="'.$Dir.'developer/index.php" id="hrefBtn">Developer Dashboard</a>';
     }
     echo '<a href="?logout" id="hrefBtn">logout</a>';
 }
