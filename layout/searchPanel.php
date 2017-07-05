469cf051ddd83dcfe0a92b169a260292d7c5029a
codemaster05330 <thomasj0830@gmail.com>
2017-07-05 22:54:01 +0300

    add upper appliction notice

--- layout/searchPanel.php
+++ layout/searchPanel.php
@@ -1,4 +1,15 @@
 <div id="searchBar">
+<div id="uppernotice">
+This Application is demo version of that project <a href=" https://github.com/mo7amed-hussein/free-apps-store-system.git" id="hrefBtn">github link</a>
+<br />
+It was developed by  <strong> mohamed hussein </strong>
+<br />
+you are welcome to contact me at
+<br />
+m.hussein1389@gmail.com
+<br />
+Have fun!
+</div>
 <form action="searchApps.php" method="get">
 <input type="text" placeholder="type to search" name="keyword"/>
 <input type="submit" name="search" value="search"/>
